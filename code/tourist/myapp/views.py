f1a561fd6c4aa2359171e081847c0870438049cd
品 <10946009@ntub.edu.tw>
2023-04-16 16:05:47 +0800

    新增台北市道路資料,搜尋景點測試

--- code/tourist/myapp/views.py
+++ code/tourist/myapp/views.py
@@ -6,6 +6,7 @@ import os
 import populartimes
 from os.path import join, dirname
 from dotenv import load_dotenv, find_dotenv
+import requests
 #在這定義函數
 
 # def sayhello(request,username):
@@ -29,3 +30,27 @@ def sayhello(request):
     print(p)
     print(time_spent)
     return render(request,"sayhello.html",locals())
+
+
+def get_all_taiwan(request):
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path, override=True)  # 設定 override 才會更新變數哦！
+    GOOGLE_PLACES_API_KEY = os.environ.get("GOOGLE_PLACES_API_KEY")
+
+
+    URL = 'https://maps.googleapis.com/maps/api/place/textsearch/json?'
+
+    #之後要用迴圈把資料抓起來(範例:"新北市中和區景平路"+"景點")
+    r = requests.get(URL + 'query=' + "台北景點" +
+                    '&key=' + GOOGLE_PLACES_API_KEY)
+
+    data = r.json()
+    test_list = []
+    for result in data['results']:
+        test_list.append([result['name'],result['formatted_address']]) 
+        print(test_list)
+        print(result['name'], result)
+        if len(test_list) == 10:
+            break
+
+    return render(request,"get_all_taiwan.html",locals())
