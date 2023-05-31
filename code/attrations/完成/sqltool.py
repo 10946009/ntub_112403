b774ed59bdc829d348dd46d703056600b0a3e6ac
品 <10946009@ntub.edu.tw>
2023-06-01 00:02:36 +0800

    修改get資料 新增input資料庫的方法

---
+++ code/attrations/完成/sqltool.py
@@ -0,0 +1,31 @@
+import psycopg2
+
+class Postgres:
+    def __init__(self):
+        self.conn = None
+        try:
+            # connect to the PostgreSQL server
+            print("Connecting to the PostgreSQL database...")
+            self.conn = psycopg2.connect(
+                host="localhost",
+                dbname="trip",
+                user="postgres",
+                password="10946009"
+            )
+        except (Exception, psycopg2.DatabaseError) as error:
+            print('有錯誤',error)
+
+    def __del__(self):
+        if self.conn is not None:
+            self.conn.close()
+            print("Database connection closed.")
+
+    def create_multi(self,query,values) -> int :
+        try:
+            cur = self.conn.cursor()
+            cur.executemany(query, values)
+            self.conn.commit()
+            cur.close()
+            return cur.rowcount
+        except (Exception, psycopg2.DatabaseError) as error:
+            print('有錯誤',error)
