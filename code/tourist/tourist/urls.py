c433b06dfff606621341323fe970ba8e56b92257
品 <10946009@ntub.edu.tw>
2023-06-26 15:27:59 +0800

    新增搜尋功能

--- code/tourist/tourist/urls.py
+++ code/tourist/tourist/urls.py
@@ -36,7 +36,7 @@ urlpatterns = [
     path('history/',views.history),
     path('favorite/',views.favorite),
     path('share/',views.share),
-    path('attraction_details/',views.attraction_details),
+    path('attraction_details/',views.attraction_details,name='search_results'),
     # path('attraction_details/<int:a_id>',views.attraction_details),
 
     # path('sayhello/<str:username>',sayhello), #新增
