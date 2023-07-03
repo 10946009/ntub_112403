30c9bed5129e4b2777daa16e4fcd97bf81af62d6
ZiYun <10946010@ntub.edu.tw>
2023-07-03 16:04:01 +0800

    新增演算法步驟4-2

--- code/tourist/myapp/views.py
+++ code/tourist/myapp/views.py
@@ -265,6 +265,7 @@ def test_input(request):
 
     # print(response)
     # ---------------------------------------------------已經抓到時間與距離(上方)
+    #2.選擇一些景點做為O（使用者選擇的景點）
     #抓取使用者所選的景點ID
     o_attractions_list = [
             "ChIJm5fgyKOuQjQR3vKgJWoDVPk",
@@ -273,13 +274,15 @@ def test_input(request):
             "ChIJ3d0-q3-uQjQR3ywoxROtSPA",
             "ChIJGbWNq1OvQjQR1HoXIbC0hF4",
         ]
+
+    # 3.根據O裡面的景點，利用tag找出相似景點並推薦（組成新的O)
     tags_same_score = []
     tags_same_score_total = []
     p_attractions_list=[]
-    
+
     # 抓o周遭的景點(需新增程式碼)
     near_o = ["ChIJwSbjBn-uQjQR_sfLRBosWGA","ChIJ45YiuLmuQjQRgmBcRZ0ludA","ChIJ_T8x36OuQjQRFrhmtfK0kZA","ChIJ3V-FBKOvQjQR3Fcd0_4_Pu8","ChIJh3JR3LGuQjQRkt167cxfWSE","ChIJIcip9zqsQjQRfMdMBF6n2-k"]
-
+    
     for n in near_o:
         
         n_db = Attractions.objects.get(place_id=n)
@@ -304,14 +307,41 @@ def test_input(request):
         p_attractions_list.append(near_o[f_max_i_list[i][0]])
     print(p_attractions_list)
 
-
-    # 3.根據O裡面的景點，利用tag找出相似景點並推薦（組成新的O)
-
+    #將使用者在p_attractions_list所選的景點加入O
+    user_select_p = [] 
+    user_select_p = p_attractions_list #抓使用者所選擇的
+    o_attractions_list += user_select_p
+    print(o_attractions_list)
     # 4.將使用者所選擇的所有景點
     #     * 根據使用者提供的資料（喜好）去判斷重複程度（如5個相似，1個相似之類的），沒有的話變成手動給(暫定)，
+    
     #     * 再判斷景點的人潮流量（1-5，5為最高），
+    now_time = 780
+    time = now_time//60
+    week = 2
+    for o in o_attractions_list:
+        o_db = Attractions.objects.get(place_id=o)
+        o_crowd_opening = o_db.crowd_opening_set.filter(week=week).values()
+        crowd = o_crowd_opening[0]["crowd"]
+        opening = o_crowd_opening[0]["opening"]
+        try:
+            crowd = o_crowd_opening[0]["crowd"][time-1]
+            # crowd = crowd_judge(crowd)
+        except:
+            print(crowd)
+            
+        print("時間:",opening,",擁擠:",crowd)
+        print("這裡",o_crowd_opening)
+    
+    
+
     #     * 最後使用normalization將兩者的區間變成[0,1]，再賦予他們權重（如0.5、0.5），最後根據分數去排序景點。
     return render(request, "_test.html")
+
+# 判斷人潮程度
+
+    
+
     
 
 #抓到的時間距離資料(參考用)
