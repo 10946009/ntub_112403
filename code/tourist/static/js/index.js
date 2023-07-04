fe31d4a3d3d91b7a5b248ad75322f36cbd83462a
ching_c <10946013@ntub.edu.tw>
2023-07-04 14:00:22 +0800

    新增我的最愛JS

--- code/tourist/static/js/index.js
+++ code/tourist/static/js/index.js
@@ -1,3 +1,22 @@
+var heart = document.getElementsByClassName('heart_icon');
+    var span = document.getElementsByClassName('count');
+    for(var i = 0;i < heart.length; i++){
+        var count;
+        (function(i){
+            var flag = true;//點擊收藏
+            heart[i].onclick = function(){
+                if(flag){
+                    flag = false;//無收藏被點擊
+                    this.className = "fa-solid fa-heart heart_icon active";
+                    ++span[i].innerHTML;
+                }else{
+                    flag = true//點擊取消收藏
+                    this.className = "fa-solid fa-heart heart_icon";
+                    --span[i].innerHTML;
+                }
+            }  
+        })(i);
+    } 
 
 $('.owl-carousel').owlCarousel({
     loop:true,
