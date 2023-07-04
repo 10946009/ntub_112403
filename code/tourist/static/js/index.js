b822928fcbb93c1b7e55d30a6a95e501462bd859
ching_c <10946013@ntub.edu.tw>
2023-07-04 17:29:57 +0800

    新增增加行程JS

--- code/tourist/static/js/index.js
+++ code/tourist/static/js/index.js
@@ -17,6 +17,59 @@ var heart = document.getElementsByClassName('heart_icon');
             }  
         })(i);
     } 
+    
+function add() {
+    document.getElementById("myDropdown").classList.toggle("show");
+    }
+      
+    // Close the dropdown menu if the user clicks outside of it
+    window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+            }
+          }
+        }
+      }    
+    
+function add1() {
+    document.getElementById("myDropdown1").classList.toggle("show");
+    }
+      
+    // Close the dropdown menu if the user clicks outside of it
+    window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+            }
+          }
+        }
+      }   
+function add2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+    }
+      
+    // Close the dropdown menu if the user clicks outside of it
+    window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+            }
+          }
+        }
+      }   
 
 $('.owl-carousel').owlCarousel({
     loop:true,
