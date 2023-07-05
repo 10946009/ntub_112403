d09b43e11fc4507e6c201d77ce4ace3bf9a0a043
品 <10946009@ntub.edu.tw>
2023-07-05 22:57:05 +0800

    新增將額外的景點資料寫入json檔

--- code/tourist/myapp/完成/check_opendata.py
+++ code/tourist/myapp/完成/check_opendata.py
@@ -16,41 +16,35 @@ import json
 #     if data['Region'] == "臺北市":
 #         print(data['Town'])
 
-
-
 # 取得基本資料json
-def get_textsearch(GOOGLE_PLACES_API_KEY,address,a_type):
-    URL = "https://maps.googleapis.com/maps/api/place/textsearch/json?"
-    response = requests.get(URL + "query=" + f'{address[0]}{address[1]}{a_type}' + '&language=zh-TW'+"&key=" + GOOGLE_PLACES_API_KEY)
-    data = response.content.decode('unicode_escape')
-    data = data.replace('"<a href=', '')
-    data = re.sub(r'>.*</a>"', '', data)
+# def get_textsearch(GOOGLE_PLACES_API_KEY,a_name):
+#     URL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?"
+#     response = requests.get(URL + "input=" + "臺北市" +a_name + "&inputtype=textquery&fields=place_id%2Cformatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry%2Cuser_ratings_total%2Cphoto%2Ctype" +"&key=" + GOOGLE_PLACES_API_KEY)
+#     # response = requests.get(URL + "input=" + a_name + '&language=zh-TW'+"&key=" + GOOGLE_PLACES_API_KEY)
+#     data = response.content.decode('unicode_escape')
+#     data = data.replace('"<a href=', '')
+#     data = re.sub(r'>.*</a>"', '', data)
+#     return data
 
-    f = open(os.getcwd()+f'/{address[0]}{address[1]}{a_type}.json', 'a',encoding='utf-8')
-    f.write(data)
-    f.close()
-
-
-with open('opendata.csv',newline='',encoding='utf-8')as csvfile:
-    address_list = list(csv.reader(csvfile))
-    list(address_list)
-    for a_name in address_list:
-        if f'{address[0]}{address[1]}{a_type}.json' in os.listdir():
-            print(f'{address[0]}{address[1]}{a_type}.json','已存在')
-        else:
-            print('產生',f'{address[0]}{address[1]}{a_type}.json')
-            get_textsearch(GOOGLE_PLACES_API_KEY,address,a_type)
-
-        filename = f'{os.getcwd()}/{address[0]}{address[1]}{a_type}.json'
-
-        if f'{address[0]}{address[1]}{a_type}擁擠資訊.json' in os.listdir():
-            print(f'{address[0]}{address[1]}{a_type}擁擠資訊.json','已存在')
-        else: 
-            print('產生',f'{address[0]}{address[1]}{a_type}擁擠資訊.json')
-            get_populartimes(filename,GOOGLE_PLACES_API_KEY,a_type)
-
-        if f'{address[0]}{address[1]}{a_type}營業時間.json' in os.listdir():
-            print(f'{address[0]}{address[1]}{a_type}營業時間.json','已存在')
-        else: 
-            print('產生',f'{address[0]}{address[1]}{a_type}營業時間.json')
-            get_details(filename,GOOGLE_PLACES_API_KEY,a_type) 
+# 將景點資料寫入json檔
+# with open('opendata.csv',newline='',encoding='utf-8')as csvfile:
+#     address_list = list(csv.reader(csvfile))
+#     # list(address_list)
+#     result = []
+#     try:
+#         for a_name in address_list:
+#             data = get_textsearch(GOOGLE_PLACES_API_KEY,a_name[0])
+#             json_data = json.loads(data)
+#             print(a_name[0],",新增的：",json_data['candidates'][0]["name"])
+#             result.append(json_data)
+#     except:
+#         print(a_name[0],"沒抓到")
+#         # print("這QQQ",data)
+#     # print("我在這",result)
+#     f = open(os.getcwd()+f'/opendata.json', 'w',encoding='utf-8')
+#     f.write(json.dumps(result))
+#     f.close()
+# 還沒寫好
+        # filename = f'{os.getcwd()}/{address[0]}{address[1]}{a_type}.json'
+        # get_populartimes(filename,GOOGLE_PLACES_API_KEY,a_type)
+        # get_details(filename,GOOGLE_PLACES_API_KEY,a_type)
