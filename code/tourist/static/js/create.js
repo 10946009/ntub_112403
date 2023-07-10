271f260afc2660dd30981e3a0804c8e503be0534
ching_c <10946013@ntub.edu.tw>
2023-07-10 15:57:01 +0800

    增加收藏、加行程JS&angular

--- code/tourist/static/js/create.js
+++ code/tourist/static/js/create.js
@@ -16,6 +16,74 @@ $(function(){
   });
 });
 
+var heart = document.getElementsByClassName('heart_icon');
+    // var span = document.getElementsByClassName('count');
+    for(var i = 0;i < heart.length; i++){
+        // var count;
+        (function(i){
+            var flag = true;//點擊收藏
+            heart[i].onclick = function(){
+                if(flag){
+                    flag = false;//無收藏被點擊
+                    this.className = "fa-solid fa-heart heart_icon active";
+                    // ++span[i].innerHTML;
+                }else{
+                    flag = true//點擊取消收藏
+                    this.className = "fa-solid fa-heart heart_icon";
+                    // --span[i].innerHTML;
+                }
+            }  
+        })(i);
+    } 
+
+function add() {
+    document.getElementById("myDropdown").classList.toggle("show");
+    }
+    // Close the dropdown menu if the user clicks outside of it
+    window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+            }
+          }
+        }
+      } 
+function add1() {
+    document.getElementById("myDropdown1").classList.toggle("show");
+    }
+    // Close the dropdown menu if the user clicks outside of it
+    window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+            }
+          }
+        }
+      } 
+function add2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+    }
+    // Close the dropdown menu if the user clicks outside of it
+    window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+            }
+          }
+        }
+      } 
 
 var dialog,x;
 window.onload=function(){
