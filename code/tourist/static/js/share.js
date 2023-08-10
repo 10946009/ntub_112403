8143d4f7d1eec384ee77aeb33129f40e5fae3a39
GomiUc <10946003@ntub.edu.tw>
2023-08-10 22:47:42 +0800

    增加動畫(未能正確執行)

--- code/tourist/static/js/share.js
+++ code/tourist/static/js/share.js
@@ -1,15 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const deleteIcons = document.querySelectorAll(".delete-icon");
-
-  deleteIcons.forEach(icon => {
-    icon.addEventListener("click", function() {
-      const card = this.closest(".card");
-      card.classList.add("shake");
-
-      // 移除抖動效果以重置動畫
-      setTimeout(() => {
-        card.classList.remove("shake");
-      }, 500); // 等待抖動動畫完成
-    });
-  });
+document.getElementById("trash-icon").addEventListener("click", function() {
+  this.classList.toggle("clicked");
 });
