47718926639cadb4de3cb0b0ede030403b615c52
品 <10946009@ntub.edu.tw>
2023-08-16 02:42:31 +0800

    修改行程詳細資訊和html加入收藏按鈕的判斷

--- code/tourist/static/js/addFavorite.js
+++ code/tourist/static/js/addFavorite.js
@@ -1,6 +1,14 @@
 function addFavorite(aid){
+    var favoriteButton = document.getElementById("submitfavorite" + aid);
+    var isFavoriteValue = favoriteButton.getAttribute("data-isfavorite");
+    if (isFavoriteValue === "1") {
+        favoriteButton.textContent = "加入收藏";
+        favoriteButton.setAttribute("data-isfavorite", "0");
+    } else {
+        favoriteButton.textContent = "取消收藏";
+        favoriteButton.setAttribute("data-isfavorite", "1");
+    }
     const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
-    alert( csrftoken )
     $.ajax({
         headers: {'X-CSRFToken': csrftoken},
         type: "POST",
@@ -16,8 +24,3 @@ function addFavorite(aid){
         }
     });
 }
-
-// const form = document.getElementById('addFavorite')
-// form.addEventListener('submit', () => {
-//     event.preventDefault() // 新增此行
-//   })
