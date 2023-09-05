939f68c9a0a925a52918837bb7d6e4cc81244ba2
ching_c <10946013@ntub.edu.tw>
2023-09-05 19:34:35 +0800

    修改頁簽JS(不知道為啥沒辦法顯示內容

--- code/tourist/static/js/history.js
+++ code/tourist/static/js/history.js
@@ -1,47 +1,100 @@
 /*展開詳細行程more*/
-// 获取所有的 "詳細資料" 按钮
-// const detailButtons = document.querySelectorAll(".more_btn");
-// const openmore = document.getElementsByClassName(".openmore");
 
-// // 为每个按钮添加点击事件监听
-// detailButtons.forEach(button => {
-//   button.addEventListener("click", function() {
-//     const targetId = button.getAttribute("data-target");
-//     const targetDiv = document.getElementById(targetId);
+document.addEventListener("DOMContentLoaded", function(){
+  
+  
+  // 選擇所有的按鈕和對應的內容區域
+  const buttons = document.querySelectorAll(".more_btn");
+  const openmoreDivs = document.querySelectorAll(".openmore");
+
+  // 設置每個按鈕的點擊事件監聽器
+  buttons.forEach((button, index) => {
+      button.addEventListener("click", (event) => {
+          // 阻止錨點的默認跳轉行為
+          event.preventDefault();
+
+          // 找到按鈕對應的 data-target 屬性的值
+          const targetID = button.getAttribute("data-target");
+
+          // 找到對應的內容區域
+          const targetDiv = document.getElementById(targetID);
+
+          // 隱藏所有內容區域
+          openmoreDivs.forEach((div) => {
+              div.style.display = "none";
+          });
+
+          // 顯示點擊按鈕對應的內容區域
+          if (targetDiv) {
+              targetDiv.style.display = "block";
+          }
+
+          // 將按鈕移到其下方的內容區域
+          const parentDiv = button.closest(".mytravel");
+          if (parentDiv) {
+              parentDiv.parentNode.insertBefore(targetDiv, parentDiv.nextSibling);
+          }
+      });
+  });
+
+// 選擇所有的內容區域中的連結
+const tabLinks = document.querySelectorAll(".nav-link");
+
+// 設置每個連結的點擊事件監聽器
+tabLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+      // 阻止錨點的默認跳轉行為
+      event.preventDefault();
+
+      // 找到連結對應的 data-tab 屬性的值
+      const tabID = link.getAttribute("data-tab");
+
+      // 找到對應的內容區域
+      const content = document.getElementById(tabID);
+
+      if (content) {
+          // 隱藏所有內容區域
+          const allContent = content.closest("#more3").querySelectorAll(".content");
+          allContent.forEach((c) => {
+              c.style.display = "none";
+          });
+
+          // 顯示點擊連結對應的內容
+          content.style.display = "block";
+      }
+  });
+});
+
+});
+
+
+
+// const moreButton = document.querySelector(".more_btn");
+// const openmore = document.querySelector(".openmore");
+// const tabs = document.querySelectorAll(".nav-link");
+// const contents = document.querySelectorAll(".content");
+
+// moreButton.addEventListener("click", function () {
+//   if (openmore.style.display === "none" || openmore.style.display === "") {
+//     openmore.style.display = "block";
+//   } else {
+//     openmore.style.display = "none";
+//   }
+// });
+
+// tabs.forEach(tab => {
+//   tab.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     // 隐藏所有内容
+//     contents.forEach(content => {
+//       content.style.display = "none";
+//     });
     
-//     if (targetDiv) {
-//       // 切换目标 div 的显示状态
-//       targetDiv.classList.toggle("hidden-details");
+//     // 显示与点击标签相关的内容
+//     const tabId = this.getAttribute("data-tab");
+//     const contentToShow = document.getElementById("content" + tabId);
+//     if (contentToShow) {
+//       contentToShow.style.display = "block";
 //     }
 //   });
 // });
-
-const moreButton = document.querySelector(".more_btn");
-const openmore = document.querySelector(".openmore");
-const tabs = document.querySelectorAll(".nav-link");
-const contents = document.querySelectorAll(".content");
-
-moreButton.addEventListener("click", function () {
-  if (openmore.style.display === "none" || openmore.style.display === "") {
-    openmore.style.display = "block";
-  } else {
-    openmore.style.display = "none";
-  }
-});
-
-tabs.forEach(tab => {
-  tab.addEventListener("click", function (event) {
-    event.preventDefault();
-    // 隐藏所有内容
-    contents.forEach(content => {
-      content.style.display = "none";
-    });
-    
-    // 显示与点击标签相关的内容
-    const tabId = this.getAttribute("data-tab");
-    const contentToShow = document.getElementById("content" + tabId);
-    if (contentToShow) {
-      contentToShow.style.display = "block";
-    }
-  });
-});
