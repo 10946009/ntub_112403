fd4580ced95758560813c4547ef631927b235122
ZiYun <10946010@ntub.edu.tw>
2023-09-09 15:02:18 +0800

    新增首頁熱門景點連接資料庫

--- code/tourist/myapp/views.py
+++ code/tourist/myapp/views.py
@@ -119,6 +119,27 @@ def admin_comment(request):
 
 # 首頁
 def index(request):
+    # 抓熱門資料
+    # hot_result = Attractions.objects.order_by('rating', 'rating_total').values()[:9]
+    hot =[57,79,100,199,216,421,450,454,713]
+    hot_result = Attractions.objects.filter(id__in=hot).values()
+    # 讓資料3個一組
+    temp_hot=[]
+    hotAttractionsList=[]
+    for i in hot_result:
+        temp_hot.append(i)
+        if len(temp_hot)==3:
+            hotAttractionsList.append(temp_hot)
+            temp_hot=[]
+    if len(temp_hot) !=0:
+        hotAttractionsList.append(temp_hot)
+        temp_hot=[]
+
+    print(hotAttractionsList)
+    
+    # hot =[57,79,100,199,216,421,450,454,713]
+    # for a o in hot:
+    #     aDb = Attractions.objects.get()
     return render(request, "index.html", locals())
 
 
