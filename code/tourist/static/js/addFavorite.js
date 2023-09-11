0a63b333111b141a01453583c9b44143853ebbf1
JIA-LING <10946029@ntub.edu.tw>
2023-09-12 00:12:54 +0800

    更改收藏按鈕形狀

--- code/tourist/static/js/addFavorite.js
+++ code/tourist/static/js/addFavorite.js
@@ -2,10 +2,10 @@ function addFavorite(aid) {
     var favoriteButton = document.getElementById("submitfavorite" + aid);
     var isFavoriteValue = favoriteButton.getAttribute("data-isfavorite");
     if (isFavoriteValue === "1") {
-        favoriteButton.innerHTML = '<i class="fa-regular fa-bookmark"></i>&ensp;收藏';
+        favoriteButton.innerHTML = '<i class="fa-regular fa-heart"></i>&ensp;收藏';
         favoriteButton.setAttribute("data-isfavorite", "0");
     } else {
-        favoriteButton.innerHTML = '<i class="fa-solid fa-bookmark"></i>&ensp;已收藏';
+        favoriteButton.innerHTML = '<i class="fa-solid fa-heart"></i>&ensp;已收藏';
         favoriteButton.setAttribute("data-isfavorite", "1");
     }
     const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
