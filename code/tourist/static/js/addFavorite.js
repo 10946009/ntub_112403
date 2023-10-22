6bfe353c9e9d87f99b562bdc14dd6c50eba77243
ZiYun <10946010@ntub.edu.tw>
2023-10-23 00:24:51 +0800

    新增首頁熱門行程收藏功能(JS)

--- code/tourist/static/js/addFavorite.js
+++ code/tourist/static/js/addFavorite.js
@@ -31,15 +31,16 @@ function addFavorite(aid) {
     });
 }
 
-function addFavorite_index(aid) {
-    console.log("目前點擊的id為" + aid);
+function addFavorite_index(id,type) {
+    console.log("目前點擊的id為" + id);
+    console.log("目前URL為" + type);
     const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
     $.ajax({
         headers: { 'X-CSRFToken': csrftoken },
         type: "POST",
-        url: "/add_favorite/",
+        url: type,
         data: {
-            'aid': aid,
+            'id': id,
         },
         success: function (response) {
             
