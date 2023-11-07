2e0524c53e11f0af3fa38ddebac9f65f9baae4e9
ching_c <10946013@ntub.edu.tw>
2023-11-07 17:53:49 +0800

    加上點擊打開右半邊的功能("這邊好像抓不到id名稱所以JS不會動"有加這個註解
    下面有我的範例

--- code/tourist/static/js/history.js
+++ code/tourist/static/js/history.js
@@ -1,17 +1,43 @@
-// 按鈕變文字
-function clickOpen(button) {
-  const isDetails = button.dataset.isDetails === 'true' || false;
+// // 按鈕變文字
+// function clickOpen(button) {
+//   const isDetails = button.dataset.isDetails === 'true' || false;
 
-  if (!isDetails) {
-    button.textContent = "關閉行程";
-    button.style.backgroundColor = "#F55";
-    button.dataset.isDetails = 'true';
+//   if (!isDetails) {
+//     button.textContent = "關閉行程";
+//     button.style.backgroundColor = "#F55";
+//     button.dataset.isDetails = 'true';
+//   } else {
+//     button.textContent = "詳細行程";
+//     button.style.backgroundColor = "rgb(255, 240, 126)";
+//     button.dataset.isDetails = 'false';
+//   }
+// }
+
+
+function clickShow(button) {
+  const targetBtn = button.getAttribute('data-target');
+  const showDiv = document.getElementById(targetBtn);
+
+  if (showDiv.style.display === 'block') {
+      showDiv.style.display = 'none';
+      button.textContent = '詳細行程';
   } else {
-    button.textContent = "詳細行程";
-    button.style.backgroundColor = "rgb(255, 240, 126)";
-    button.dataset.isDetails = 'false';
+      document.querySelectorAll("[id^='show']").forEach(div => {
+          div.style.display = "none";
+      })
+      showDiv.style.display = 'block';
+      button.textContent = '關閉閱覽';
   }
+  // 获取所有左侧按钮并更改非当前点击按钮的文本为 "点我出现"
+  const allButtons = document.querySelectorAll("[id^='clickme']");
+  allButtons.forEach(btn => {
+      if (btn !== button) {
+          btn.textContent = '詳細行程';
+      }
+  });
 }
+
+
 
 $(document).ready(function () {
 
@@ -24,7 +50,7 @@ $(document).ready(function () {
   });
 
   // 设置初始选中链接的样式
-  $(".shareFilter_btn").each(function() {
+  $(".shareFilter_btn").each(function () {
     if ($(this).attr("href") === currentwin) {
       $(this).addClass("shareFilter_btn_click");
     }
