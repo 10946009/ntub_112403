1a30a2b8f98911c78ce5dabac75df7765f1b2230
品 <10946009@ntub.edu.tw>
2023-11-15 19:33:57 +0800

    修正BUG與相似景點問題

--- code/tourist/myapp/views/get_attractions.py
+++ code/tourist/myapp/views/get_attractions.py
@@ -6,10 +6,17 @@ from django.template.loader import render_to_string
 
 def get_attractions_data(request):
     data = None
-    if  request.GET.get('aidlist') :
+    if request.GET.get('aidlist'):
         aidlist = request.GET.get('aidlist')
-        print('aidlist',aidlist)
-        aidlist = map(int,aidlist.split(','))
+        print('aidlist', aidlist)
+        
+        if ',' in aidlist:
+            aidlist = map(int, aidlist.split(','))
+        else:
+            aidlist = [int(aidlist)]
+            
         data = Attractions.objects.filter(id__in=aidlist)
 
     return render(request, 'create_bottom.html', {'data': data})
+
+
