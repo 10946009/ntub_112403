ed7b245f8add4bc99e255bbb9818a49428086303
ching_c <10946013@ntub.edu.tw>
2023-11-18 01:59:07 +0800

    評論JS增刪改功能

--- code/tourist/static/js/history.js
+++ code/tourist/static/js/history.js
@@ -62,46 +62,34 @@ function clickShow(button) {
 // 點新增評論按鈕
 const addCommentBtn = document.getElementById('addCommentBtn');
 const addCommentSpace = document.getElementById('addCommentSpace');
+const addCTXT = document.getElementById('addCTXT');
 
 let isaddCommentSpaceVisible = false;
 let isEdit = false;
 
 function addComment() {
-  if (!isaddCommentSpaceVisible) {
+  console.log(1);
+  addCTXT.value = '';
+  if (addCommentBtn.textContent === '新增評論') {
     addCommentSpace.style.display = 'block';
     addCommentBtn.textContent = '取消評論';
-  } else {
+  } else if(addCommentBtn.textContent === '取消評論') {
     addCommentSpace.style.display = 'none';
     addCommentBtn.textContent = '新增評論';
-
   }
   isaddCommentSpaceVisible = !isaddCommentSpaceVisible;
-}
-
-
-// Change editedComment function to only handle edit initiation
-function editedComment() {
-  var commentTxt = document.querySelector('.row.editing .otherCommentTxt').innerText;
-  var editedComment = document.querySelector('.row.editing');
-
-  document.getElementById('addCTXT').value = commentTxt;
-  addCommentSpace.style.display = 'block';
-  addCommentBtn.textContent = '取消評論';
-
-  editedComment.classList.add('editing');
-  isEdit = true;
 }
 
 // 發送評論
 function submitComment() {
   var commentText = document.getElementById('addCTXT').value;
   addCommentBtn.style.display = 'none';
+  addCommentBtn.textContent = '新增評論';
 
   var newCreateComment = createCommentElement(commentText);
   insertComment(newCreateComment);
-  var editedComment = document.querySelector('.row.editing');
-  editedComment.querySelector('.otherCommentTxt').innerText = commentText;
 
+  console.log('2');
   // 創建新評論
   function createCommentElement(commentText) {
     var newComment = document.createElement('div');
@@ -122,29 +110,37 @@ function submitComment() {
 
     var iconDiv = document.createElement('div');
     iconDiv.className = 'c_editDiv';
-    // 加入該class的css(因為是創建的所以抓不到?)
     iconDiv.classList.add('c_editDiv');
 
-    var c_edit_iconDiv = document.createElement('div');
-    c_edit_iconDiv.className = 'c_edit_iconDiv';
-    var c_edit_icon = document.createElement('button');
-    c_edit_icon.className = 'fa-solid fa-pen-to-square c_edit_icon c_icon';
-    c_edit_iconDiv.appendChild(c_edit_icon);
-    iconDiv.appendChild(c_edit_iconDiv);
-    textDiv.appendChild(iconDiv);
-    // 加入該class的css(因為是創建的所以抓不到?)
-    c_edit_iconDiv.classList.add('c_icon');
+    // 新增編輯按鈕
+    var editBtnDiv = document.createElement('div');
+    editBtnDiv.className = 'c_edit_iconDiv';
+    var editBtn = document.createElement('button');
+    editBtn.className = 'fa-solid fa-pen-to-square c_edit_icon c_icon';
+    editBtn.onclick = function () {
+      editComment(textSpan);
+      // iconDiv.style.display = 'none';
+    };
+    editBtnDiv.appendChild(editBtn);
+    iconDiv.appendChild(editBtnDiv);
 
-    var c_delete_iconDiv = document.createElement('div');
-    c_delete_iconDiv.className = 'c_delete_iconDiv';
-    var c_delete_icon = document.createElement('button');
-    c_delete_icon.className = 'fa-solid fa-trash c_delete_icon c_icon';
-    c_delete_iconDiv.appendChild(c_delete_icon);
-    iconDiv.appendChild(c_delete_iconDiv);
+    // 新增刪除按鈕
+    var deleteBtnDiv = document.createElement('div');
+    deleteBtnDiv.className = 'c_delete_iconDiv';
+    var deleteBtn = document.createElement('button');
+    deleteBtn.className = 'fa-solid fa-trash c_delete_icon c_icon';
+    deleteBtn.onclick = function () {
+      if (confirm('確定要刪除該留言嗎？')) {
+        newComment.parentNode.removeChild(newComment);
+        addCommentBtn.style.display = 'block';
+        console.log(6);
+      }
+    };
+    deleteBtnDiv.appendChild(deleteBtn);
+    iconDiv.appendChild(deleteBtnDiv);
+
     textDiv.appendChild(iconDiv);
-    // 加入該class的css(因為是創建的所以抓不到?)
-    c_delete_iconDiv.classList.add('c_icon');
-    
+
     newComment.appendChild(textDiv);
     return newComment;
   }
@@ -161,12 +157,84 @@ function submitComment() {
     } else {
       commentSection.appendChild(newComment);
     }
-    isEdit = false;
     document.getElementById('addCommentSpace').style.display = 'none';
-    document.getElementById('addCommentBtn').textContent = '編輯評論'
   }
 
 }
+// 編輯按鈕
+function editComment(commentSpan) {
+  var originalText = commentSpan.innerText;
+
+  commentSpan.style.display = 'none';
+
+  var editTextArea = document.createElement('textarea');
+  editTextArea.value = originalText;
+  editTextArea.style.width = '100%';
+  editTextArea.style.height = '200px';
+
+  var buttonsContainer = document.createElement('div'); // 創建容器元素
+  buttonsContainer.style.display = 'flex'; // 使用 Flex 排列元素
+  buttonsContainer.style.flexDirection = 'column'; // 垂直排列元素
+
+  var saveEditBtn = document.createElement('button');
+  saveEditBtn.textContent = '儲存';
+  saveEditBtn.classList.add('saveBtn');
+  saveEditBtn.onclick = function () {
+    commentSpan.innerText = editTextArea.value;
+    commentSpan.parentNode.removeChild(editTextArea);
+    commentSpan.parentNode.removeChild(buttonsContainer); 
+    commentSpan.style.display = 'block';
+    editBtn.style.display = 'block';
+    deleteBtn.style.display = 'block';
+  };
+
+  var cancelEditBtn = document.createElement('button');
+  cancelEditBtn.textContent = '取消';
+  cancelEditBtn.classList.add('cancelBtn');
+  cancelEditBtn.onclick = function () {
+    commentSpan.innerText = originalText;
+    commentSpan.style.display = 'block';
+    commentSpan.parentNode.removeChild(editTextArea);
+    commentSpan.parentNode.removeChild(buttonsContainer); 
+    editBtn.style.display = 'inline-block';
+    deleteBtn.style.display = 'inline-block';
+  };
+
+  buttonsContainer.appendChild(saveEditBtn);
+  buttonsContainer.appendChild(cancelEditBtn); 
+
+  commentSpan.parentNode.insertBefore(editTextArea, commentSpan);
+  commentSpan.parentNode.appendChild(buttonsContainer); // 將容器插入 DOM 中
+
+  var deleteBtn = commentSpan.parentNode.querySelector('.c_delete_icon');
+  deleteBtn.style.display = 'none';
+  var editBtn = commentSpan.parentNode.querySelector('.c_edit_icon');
+  editBtn.style.display = 'none';
+}
+
+
+
+// function editComment(){
+//   console.log(3);
+//   var submitedComment = document.getElementById('otherCommentTxt');
+//   var editSubmitedComment = submitedComment.innerText;
+
+//   var editTextarea = document.createElement('textarea');
+//   editTextarea.value = editSubmitedComment;
+
+//   var submitButton = document.createElement('button');
+//   submitButton.innerText = '提交';
+//   submitButton.onclick = function() {
+//       var updatedText = editTextarea.value;
+//       var newSpan = document.createElement('span');
+//       newSpan.innerText = updatedText;
+
+//       editTextarea.parentNode.replaceChild(newSpan, editTextarea);
+//   };
+//   // 替換原本的 span 和添加提交按鈕
+//   submitedComment.parentNode.replaceChild(editTextarea, submitedComment);
+//   editTextarea.parentNode.appendChild(submitButton);
+// }
 
 
 $(document).ready(function () {
@@ -186,7 +254,7 @@ $(document).ready(function () {
   });
 })
 
-// 顯示目前市第幾張圖(好像沒辦法放在圖片的右下角)
+// 顯示目前是第幾張圖
 document.addEventListener("DOMContentLoaded", function () {
   var carousel = document.getElementById('carouselExampleRide');
   var counter = document.getElementById('imgCounter');
