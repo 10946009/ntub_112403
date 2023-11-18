13416ea2600f600d345bbb17c2f81d1b0a73fcba
ching_c <10946013@ntub.edu.tw>
2023-11-19 02:10:45 +0800

    redesign open and close(do not complete

--- code/tourist/static/js/history.js
+++ code/tourist/static/js/history.js
@@ -34,23 +34,26 @@ function add_favorite_share(ctid) {
 };
 
 function clickShow(button) {
-  const targetBtn = button.getAttribute('data-target');
-  const showDiv = document.getElementById(targetBtn);
+  const targetDivId = button.getAttribute('data-target');
+  const targetDiv = document.getElementById(targetDivId);
 
-  if (showDiv.style.display === 'block') {
-    showDiv.style.display = 'none';
+  if (targetDiv.style.display === 'block') {
+    targetDiv.style.display = 'none';
     button.textContent = '詳細行程';
     button.style.backgroundColor = "rgb(255, 240, 126)";
   } else {
-    document.querySelectorAll("[id^='show']").forEach(div => {
-      div.style.display = "none";
-    })
-    showDiv.style.display = 'block';
+    // 隱藏所有類似的詳細資訊 div
+    document.querySelectorAll(".openDetailDiv").forEach(div => {
+      div.style.display = 'none';
+    });
+    // 顯示目標詳細資訊 div
+    targetDiv.style.display = 'block';
     button.textContent = '關閉閱覽';
     button.style.backgroundColor = "#F55";
   }
-  // 获取所有左侧按钮并更改非当前点击按钮的文本为 "点我出现"
-  const allButtons = document.querySelectorAll("[id^='clickme']");
+
+  // 更改其他按鈕的文字狀態
+  const allButtons = document.querySelectorAll("[class^='more_btn']");
   allButtons.forEach(btn => {
     if (btn !== button) {
       btn.textContent = '詳細行程';
@@ -58,6 +61,7 @@ function clickShow(button) {
     }
   });
 }
+
 
 // 點新增評論按鈕
 const addCommentBtn = document.getElementById('addCommentBtn');
