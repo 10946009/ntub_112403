51786e843dacdd98ad84b8f8f7015a29be8f9aeb
品 <10946009@ntub.edu.tw>
2023-11-18 21:43:41 +0800

    修改輪播

--- code/tourist/static/js/owlStart.js
+++ code/tourist/static/js/owlStart.js
@@ -1,23 +1,28 @@
 // 輪播套件
 function owlStart(){
     $('.owl-carousel').owlCarousel({
-        items: 4,
-        loop: true,
+        items: 6,
+        loop: false,
         margin: 10,
         dots: false,
         nav: true,
         responsive: {
             0: {
-                items: 1
+                items: 1,
+                slideBy:1
             },
             576: {
-                items: 2
+                items: 2,
+                slideBy:2
             },
             600: {
-                items: 3
+                items: 3,
+                slideBy:3
+                
             },
             1000: {
-                items: 4
+                items: 5,
+                slideBy:5
             }
         }
     });
