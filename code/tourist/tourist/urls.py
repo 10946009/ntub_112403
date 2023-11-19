e63031257995387e79de274d3034670971379761
品 <10946009@ntub.edu.tw>
2023-11-19 20:48:24 +0800

    修正BUG

--- code/tourist/tourist/urls.py
+++ code/tourist/tourist/urls.py
@@ -80,7 +80,7 @@ urlpatterns = [
     # path('test/',views.test_input),
     path("useredit/", user_edit.user_edit),
     path("useredit/post/", user_edit.user_edit_form,name="user_post"),
-    # path("useredit/pwd/", user_edit.user_edit_pwd,name="user_pwd"),
+    path("useredit/pwd/", user_edit.user_edit_pwd,name="user_pwd"),
     path("useredit/change_favorite/", user_edit.change_favorite),
     path("favorite/", favorite.favorite),
     path("add_travel_favorite/", favorite.add_travel_favorite),  # 沒有頁面
