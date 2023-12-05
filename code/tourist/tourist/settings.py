eaf64c2b3f2746526844ed6226f9035e10d5a549
品 <10946009@ntub.edu.tw>
2023-12-05 18:33:39 +0800

    下雨串天氣

--- code/tourist/tourist/settings.py
+++ code/tourist/tourist/settings.py
@@ -80,7 +80,8 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "myapp.context_processors.get_all_attractions"
+                "myapp.context_processors.get_all_attractions",
+                "myapp.context_processors.get_now_weather"
             ],
         },
     },
